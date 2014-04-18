87801e2b076f7c6324fe9bc00cfd25b2d4ab663f
Jim Gay <jim@saturnflyer.com>
2014-04-18 15:53:36 -0400

    remove the custom runner

--- lib/selenium-connect-browserstack.rb
+++ lib/selenium-connect-browserstack.rb
@@ -2,10 +2,7 @@ require 'selenium_connect_browserstack/configuration'
 require 'selenium_connect/configuration'
 SeleniumConnect::Configuration.send(:prepend, SeleniumConnectBrowserstack::Configuration)
 
-require 'selenium_connect_browserstack/runner'
 require 'selenium_connect/runner'
-SeleniumConnect::Runner.send(:prepend, SeleniumConnectBrowserstack::Runner)
-
 # SeleniumConnect doesn't have a way to hook your own runner in without this.
 require 'selenium_connect_browserstack/runner_fix'
 SeleniumConnect::Runner.send(:prepend, SeleniumConnectBrowserstack::RunnerFix)
