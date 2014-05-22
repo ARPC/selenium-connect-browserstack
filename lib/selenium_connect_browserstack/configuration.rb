2e61bc093a7dd8f691b4ed074672ee4be632098d
Jim Gay <jim@saturnflyer.com>
2014-05-21 20:34:30 -0400

    define accessors for browserstack.debug

--- lib/selenium_connect_browserstack/configuration.rb
+++ lib/selenium_connect_browserstack/configuration.rb
@@ -1,5 +1,17 @@
 module SeleniumConnectBrowserstack
   module Configuration
     attr_accessor :browserstack, :debug, :os_version, :browser_version, :resolution
+
+    def self.prepended(base)
+      base.class_eval do
+        define_method('browserstack.debug') do
+          instance_variable_get("@browserstack_debug")
+        end
+
+        define_method('browserstack.debug=') do |val|
+          instance_variable_set("@browserstack_debug", val)
+        end
+      end
+    end
   end
 end
